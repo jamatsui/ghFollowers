113812cb0cc090367b52538986c20691e70ccfef
Jason <jason.matsui+jm@gmail.com>
2020-01-30 11:06:34 -0800

    Fix bug to setting color

--- GHFollowers/Custom Views/GFButton.swift
+++ GHFollowers/Custom Views/GFButton.swift
@@ -28,7 +28,7 @@ class GFButton: UIButton {
     
     private func configure() {
         layer.cornerRadius = 10
-        titleLabel?.textColor = .white
+        setTitleColor(.white, for: .normal)
         titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
         translatesAutoresizingMaskIntoConstraints = false
     }
