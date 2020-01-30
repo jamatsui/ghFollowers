db0d562ac63d2938a8317dc9b9994168987d048f
Jason <jason.matsui+jm@gmail.com>
2020-01-30 11:30:53 -0800

    Add basic collection view cell

---
+++ GHFollowers/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,52 @@
+//
+//  FollowerCell.swift
+//  GHFollowers
+//
+//  Created by jason on 1/30/20.
+//  Copyright © 2020 jason. All rights reserved.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseID = "FollowerCell"
+    
+    let avatarImageView = UIImageView()
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
+    
+}
---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,29 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by jason on 1/30/20.
+//  Copyright © 2020 jason. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    let placeHolderImage = UIImage(named: "avatar-placeholder")
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true // sets the image to not have sharp corners in addition to the layer corner radius
+        image = placeHolderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
