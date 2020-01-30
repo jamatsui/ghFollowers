d21fb8d7feb0406c22686ea691db2be1de777446
Jason <jason.matsui+jm@gmail.com>
2020-01-30 10:00:33 -0800

    Rm unused code

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -15,7 +15,6 @@ class FollowerListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
     }
 
