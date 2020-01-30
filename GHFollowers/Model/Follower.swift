2538c7ed5b5c20627081fed599acdea4ff611ee4
Jason <jason.matsui+jm@gmail.com>
2020-01-30 09:59:48 -0800

    Add models

--- GHFollowers/Model/Follower.swift
+++ GHFollowers/Model/Follower.swift
@@ -7,3 +7,8 @@
 //
 
 import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String // will convert to camel case
+}
--- GHFollowers/Model/User.swift
+++ GHFollowers/Model/User.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarURL: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
