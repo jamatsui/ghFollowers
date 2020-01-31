781adbe6df6ceb76ccc8e7053d3b250372603a96
Jason <jason.matsui+jm@gmail.com>
2020-01-30 16:47:31 -0800

    Add cache with a downloader on the image obj itself

--- GHFollowers/Custom Views/GFAvatarImageView.swift
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class GFAvatarImageView: UIImageView {
 
+    let cache = NetworkManager.shared.cache
     let placeHolderImage = UIImage(named: "avatar-placeholder")
     
     override init(frame: CGRect) {
@@ -29,6 +30,13 @@ class GFAvatarImageView: UIImageView {
     }
     
     func downloadImage(from urlString: String) {
+        
+        let cacheKey = NSString(string: urlString)
+        if let image = cache.object(forKey: cacheKey) {
+            self.image = image
+            return
+        }
+        
         guard let url = URL(string: urlString) else { return }
         let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
             guard let self = self else { return }
@@ -37,10 +45,9 @@ class GFAvatarImageView: UIImageView {
             guard let data = data else { return }
             
             guard let image = UIImage(data: data) else { return }
+            self.cache.setObject(image, forKey: cacheKey)
             
-            DispatchQueue.main.async {
-                self.image = image
-            }
+            DispatchQueue.main.async { self.image = image }
         }
         task.resume()
     }
--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -6,11 +6,12 @@
 //  Copyright © 2020 jason. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager() // singleton
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
