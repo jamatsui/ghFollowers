4583b7095d84ec2a8b42af1274c5e0c982455357
Jason <jason.matsui+jm@gmail.com>
2020-01-30 18:30:18 -0800

    Add loading view as the screen loads more followers from network

--- GHFollowers/Extensions/UIViewController+Ext.swift
+++ GHFollowers/Extensions/UIViewController+Ext.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+fileprivate var containerView: UIView! // nothing can access it unless it's in this file
+// this is to get around creating a variable for this extension
+ 
 extension UIViewController {
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
         DispatchQueue.main.async {
@@ -15,6 +18,36 @@ extension UIViewController {
             alertVC.modalPresentationStyle = .overFullScreen // avoids card style in iOS 13
             alertVC.modalTransitionStyle = .crossDissolve // fades in
             self.present(alertVC, animated: true)
+        }
+    }
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            containerView.alpha = 0.8
+        }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        activityIndicator.startAnimating()
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -42,10 +42,11 @@ class FollowerListVC: UIViewController {
     }
     
     func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
-            
             guard let self = self else { return }
-            
+            self.dismissLoadingView()
+
             switch result {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
