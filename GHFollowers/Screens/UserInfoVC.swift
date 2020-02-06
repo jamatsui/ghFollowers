1b1e2c135d6df4edacf4e7d81c73180dbe8b3aa8
Jason <jason.matsui+jm@gmail.com>
2020-02-06 07:53:18 -0800

    Add user info

---
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,17 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by jason on 2/6/20.
+//  Copyright © 2020 jason. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
