7129052c8e0d19ae9c0c103d10689549e6b4efa6
Jason <jason.matsui+jm@gmail.com>
2020-02-06 09:01:09 -0800

    Add VC

---
+++ GHFollowers/Custom Views/Views/GFUserInfoHeaderVC.swift
@@ -0,0 +1,30 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GHFollowers
+//
+//  Created by jason on 2/6/20.
+//  Copyright © 2020 jason. All rights reserved.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
