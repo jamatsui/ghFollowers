579f70ab0a768544b15a20fb2b2a59735b0c9639
Jason <jason.matsui+jm@gmail.com>
2020-02-06 14:38:25 -0800

    Add constants file

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by jason on 2/6/20.
+//  Copyright © 2020 jason. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
