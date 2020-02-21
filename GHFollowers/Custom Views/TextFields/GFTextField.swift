5d2b047af36cdbd18992ef224f1ad2a801bd313f
Jason <jason.matsui+jm@gmail.com>
2020-02-20 16:06:15 -0800

    Add clear text field option while editing

--- GHFollowers/Custom Views/TextFields/GFTextField.swift
+++ GHFollowers/Custom Views/TextFields/GFTextField.swift
@@ -36,6 +36,7 @@ class GFTextField: UITextField {
         backgroundColor = .tertiarySystemBackground
         autocorrectionType = .no
         returnKeyType = .go
+        clearButtonMode = .whileEditing
         placeholder = "Enter a username"
     }
 }
