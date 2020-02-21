58a841fcc97931210a057dd98fb9e80fde1ab121
Jason <jason.matsui+jm@gmail.com>
2020-02-20 20:03:10 -0800

    Refactor out code to subclass

--- GHFollowers/Custom Views/ViewControllers/GFAlertVC.swift
+++ GHFollowers/Custom Views/ViewControllers/GFAlertVC.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class GFAlertVC: UIViewController {
 
-    let containerView = UIView()
+    let containerView = GFAlertContainerView()
     let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
     let messageLabel = GFBodyLabel(textAlignment: .center)
     let actionButton = GFButton(backgroundColor: .systemPink, title: "Ok")
@@ -36,7 +36,7 @@ class GFAlertVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
         // Do any additional setup after loading the view.
         configureContainerView()
         configureTitleLabel()
@@ -47,12 +47,6 @@ class GFAlertVC: UIViewController {
     
     func configureContainerView() {
         view.addSubview(containerView)
-        containerView.backgroundColor = .systemBackground
-
-        containerView.layer.cornerRadius = 16
-        containerView.layer.borderWidth = 2
-        containerView.layer.borderColor = UIColor.white.cgColor
-        containerView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
---
+++ GHFollowers/Custom Views/Views/GFAlertContainerView.swift
@@ -0,0 +1,28 @@
+//
+//  GFAlertContainerView.swift
+//  GHFollowers
+//
+//  Created by jason on 2/20/20.
+//  Copyright © 2020 jason. All rights reserved.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
